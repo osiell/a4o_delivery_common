f81d18ce482190deae071f407a88d1043860043c
Christophe <xt0ph@adiczion.net>
2023-02-17 18:20:19 +0100

    Release v14.0.2

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 # contains the full copyright notices and license terms.
 {
     'name': 'Delivery Common',
-    'version': '14.0.1',
+    'version': '14.0.2',
     'author': 'Adiczion SARL',
     'category': 'Adiczion',
     'license': 'AGPL-3',
