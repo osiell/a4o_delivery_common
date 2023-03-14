1f45234e1ae4fd39d0814eb5f6b4b045a1b7ceb0
Christophe <xt0ph@adiczion.net>
2023-03-14 11:53:07 +0100

    Release v15.0.1.

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 # contains the full copyright notices and license terms.
 {
     'name': 'Delivery Common',
-    'version': '15.0.0',
+    'version': '15.0.1',
     'author': 'Adiczion SARL',
     'category': 'Adiczion',
     'license': 'AGPL-3',
