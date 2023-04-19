4f7b9019e68e05818774175ec43be5c7f51373f4
remihb <remi@hobo.live>
2023-04-19 16:01:14 +0200

    [MIG] a4o_delivery_common: Migration to 16.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 # contains the full copyright notices and license terms.
 {
     'name': 'Delivery Common',
-    'version': '15.0.3',
+    'version': '16.0.1.0.0',
     'author': 'Adiczion SARL',
     'category': 'Adiczion',
     'license': 'AGPL-3',
